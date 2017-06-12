fdcb081829ce5c03b104e802f5d656bcab5ae01f
Marcelo-h-h <marcelo-h-h@hotmail.com>
2017-06-12 16:47:59 -0300

    resolvido a movimentação para as laterais

--- PoupatempoGame/KinematicPrincipalScript.gd
+++ PoupatempoGame/KinematicPrincipalScript.gd
@@ -1,13 +1,12 @@
 extends KinematicBody2D
-
+var viradoPara = 'B'
 var MOTION_SPEED = 300
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+
 
 func _ready():
 	set_fixed_process(true)
-	set_process_input(true)
+#	set_process_input(true)
+	set_pos(Vector2(get_viewport_rect().size.width/2, get_viewport_rect().size.height/2))
 	
 func _fixed_process(delta):
 	var motion = Vector2()
@@ -24,26 +23,55 @@ func _fixed_process(delta):
 	motion = motion.normalized()*MOTION_SPEED*delta
 	move(motion)
 	
+	if(motion == Vector2(0,0)):
+			if(viradoPara == 'D'):
+				get_node("Personagem").set_animation("ParadoDir")       
+			if(viradoPara == 'E'):
+				get_node("Personagem").set_animation("ParadoEsq")
+			if(viradoPara == 'C'):
+				get_node("Personagem").set_animation("ParadoCima")
+			if(viradoPara == 'B'):
+				get_node("Personagem").set_animation("ParadoBaixo")
+	if(motion != Vector2(0, 0)):
+			if(motion.x > 3):
+				get_node("Personagem").set_animation("AndandoDir")
+				viradoPara = 'D'
+			if(motion.x < -3):
+				get_node("Personagem").set_animation("AndandoEsq")
+				viradoPara = 'E'
+			if(motion.y < -3):
+				get_node("Personagem").set_animation("AndandoCima")
+				viradoPara = 'C'
+			if(motion.y > 3):
+				get_node("Personagem").set_animation("AndandoBaixo")
+				viradoPara = 'B'
+				
 func _input(event):
-	if(event.type == InputEvent.KEY):
+	var posAtual = get_pos()
         #Player movement animation----------------------------------------
-       # var curPos = self.get_pos()
-		if(event.scancode == KEY_D):
-			get_node("Personagem").set_animation("AndandoDir")
-			self.get_tree().set_input_as_handled()
-		if(event.scancode == KEY_A):
-			get_node("Personagem").set_animation("AndandoEsq")
-		if(event.scancode == KEY_W):
-			get_node("Personagem").set_animation("AndandoCima")
-		if(event.scancode == KEY_S):
-			get_node("Personagem").set_animation("AndandoBaixo")
-			
-		if(event.scancode == KEY_D && event.pressed == false):
-			get_node("Personagem").set_animation("ParadoDir")
-		if(event.scancode == KEY_A && event.pressed == false):
-			get_node("Personagem").set_animation("ParadoEsq")
-		if(event.scancode == KEY_W && event.pressed == false):
-			get_node("Personagem").set_animation("ParadoCima")
-		if(event.scancode == KEY_S && event.pressed == false):
-			get_node("Personagem").set_animation("ParadoBaixo")
-       
+ 
+	if(event.type == InputEvent.KEY):
+#		if(event.scancode == KEY_D && event.pressed):
+#			get_node("Personagem").set_animation("AndandoDir")
+#			viradoPara = 'D'
+#		if(event.scancode == KEY_A && event.pressed):
+#			get_node("Personagem").set_animation("AndandoEsq")
+#			viradoPara = 'E'
+#		if(event.scancode == KEY_W && event.pressed):
+#			get_node("Personagem").set_animation("AndandoCima")
+#			viradoPara = 'C'
+		if(event.scancode == KEY_S && event.pressed):
+#			get_node("Personagem").set_animation("AndandoBaixo")
+			viradoPara = 'B'
+
+		#if(event.scancode == KEY_D && event.pressed == false):
+#			get_node("Personagem").set_animation("ParadoDir")
+#		if(event.scancode == KEY_A && event.pressed == false):
+#			get_node("Personagem").set_animation("ParadoEsq")
+#		if(event.scancode == KEY_W && event.pressed == false):
+#			get_node("Personagem").set_animation("ParadoCima")
+#		if(event.scancode == KEY_S && event.pressed == false):
+#			get_node("Personagem").set_animation("ParadoBaixo")
+		
+				
+		
