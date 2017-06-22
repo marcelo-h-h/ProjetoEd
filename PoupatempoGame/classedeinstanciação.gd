555d229948b61361182bcc9d4d00cca900e2569a
Marcelo-h-h <marcelo-h-h@hotmail.com>
2017-06-22 16:32:33 -0300

    modificada a estrutura da fila

--- PoupatempoGame/classedeinstanciação.gd
+++ PoupatempoGame/classedeinstanciação.gd
@@ -1,44 +1,66 @@
 extends Node2D
 
 var filadepessoas = []
-var ultimapos = Vector2(110,415)
-var numerodepessoasinicial = 20
+var ultimaPos = Vector2(110,415)
+var numerodepessoasinicial = 10
+var ultimaAge = 15
 
 func _ready():
-	print(get_child(0))
+	set_process(true)
 	var i = 0
 	while i < numerodepessoasinicial:
 		var pessoa = Sprite.new()
 		var idade = Label.new()
-		idade.set_text("18")
+		idade.set_text(String(randi() % 100))
 		pessoa.add_child(idade)
-		filadepessoas.append(pessoa)
-		filadepessoas[i].set_texture(load("res://Images/Average1/SideWalkStand.png"))
-		add_child(filadepessoas[i])
+		add_child(pessoa)
+		pessoa.set_texture(load("res://Images/Average1/SideWalkStand.png"))
+		_insere(pessoa, filadepessoas)
 		i += 1
-#	print(filadepessoas[1].get_texture())
-	for i in range(0, filadepessoas.size()):
-		filadepessoas[i].set_pos(ultimapos)
-		ultimapos = i*10*Vector2(5, 0) + Vector2(110,415)
+	get_node("tempoDeSurgimento").set_wait_time(5)
 	
-	var novapessoa = Sprite.new()
-	var labeldanovapessoa = Label.new()
-	labeldanovapessoa.set_text("78")
-	novapessoa.add_child(labeldanovapessoa)
-	add_child(novapessoa)
-	novapessoa.set_texture(load("res://Images/Average2/SideWalkStand.png"))
-	print(novapessoa.get_child(0))
-	_insere(novapessoa, filadepessoas)
-	_ready()
+#	for i in range(0, filadepessoas.size()):
+#		filadepessoas[i].set_pos(ultimaPos)
+#		ultimaPos = i*10*Vector2(5, 0) + Vector2(110,415)
+	# ---- surge nova pessoa -----------------------------------
+
+	#-----------------------------------------------------------------------
 		
+func _draw():
+	for i in range(0, filadepessoas.size()):
+		ultimaPos = i*15*Vector2(5, 0) + Vector2(110,415)
+		filadepessoas[i].set_pos(ultimaPos)
+	
+
 func _insere(umapessoa, fila):
 	if(int(umapessoa.get_child(0).get_text()) < 60):
-		print("menor de fato")
 		fila.append(umapessoa)
 	else:
 		for elemento in fila:
 			if(int(elemento.get_child(0).get_text()) < 60):
-				fila.insert (fila.find(elemento), umapessoa)
-				print("maior de fato")
+				fila.insert(fila.find(elemento), umapessoa)
 				break
+	
+func criaPessoa():
+	var novapessoa = Sprite.new()
+	var labeldanovapessoa = Label.new()
+	labeldanovapessoa.set_text(String(randi() %100))
+	novapessoa.add_child(labeldanovapessoa)
+	add_child(novapessoa)
+	#aqui vai um if para determinar a textura do personagem
+	novapessoa.set_texture(load("res://Images/Average2/SideWalkStand.png"))
+	_insere(novapessoa, filadepessoas)
+
+
+func _on_Button_pressed():
+	criaPessoa()
+	print(filadepessoas)
+	update()
+
+
+func _on_tempoDeSurgimento_timeout():
+	get_node("tempoDeSurgimento").start()
+	criaPessoa()
+	update()
+	print("TIMEOUT")
 	
